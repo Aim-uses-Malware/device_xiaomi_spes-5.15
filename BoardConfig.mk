7c166137a322d1f0fe553b88b8333a8b71b56876
Guidix <guidixm@gmail.com>
2025-08-16 15:19:14 +0300

    spes: Drop power mode extension to handle dt2w
    WE don't have it
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -199,10 +199,6 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/configs/props/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
 
-# Power
-TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
-TARGET_TAP_TO_WAKE_NODE := "/proc/tp_gesture"
-
 # Recovery
 BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
 BOARD_INCLUDE_RECOVERY_RAMDISK_IN_VENDOR_BOOT := true
