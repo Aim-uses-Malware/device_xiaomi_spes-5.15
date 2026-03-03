1cb026be05123d572fc319b5f47e2dedc13d27fe
HALt <102590462+halt-spesn@users.noreply.github.com>
2026-03-03 10:25:53 +0200

    Add ST21 NFC driver to BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,7 +137,8 @@ TARGET_KERNEL_EXT_MODULES := \
         qcom/opensource/wlan/qcacld-3.0 \
         qcom/opensource/bt-kernel \
         drivers/input/fingerprint/fpc \
-        drivers/input/fingerprint/goodix
+        drivers/input/fingerprint/goodix \
+        drivers/st21nfc
 
 BOOT_KERNEL_MODULES := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.vendor_dlkm))
